e374767cdada4e547efc812f584f88191204f4ce
tiago.schmidt@dbccompany.com.br <tiago.schmidt@dbccompany.com.br>
2022-01-29 19:14:23 -0300

    feat: added some config files and changed global font-style.

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,31 @@
+const GlobalStyle = () => {
+	return (
+		<style global jsx>
+			{`
+				* {
+					margin: 0;
+					padding: 0;
+					box-sizing: border-box;
+					list-style: none;
+				}
+				body {
+					font-family: 'Bungee', sans-serif;
+				}
+				/* App fit Height */ 
+				html, body, #__next {
+					min-height: 100vh;
+					display: flex;
+					flex: 1;
+				}
+				#__next {
+					flex: 1;
+				}
+				#__next > * {
+					flex: 1;
+				}
+			`}
+		</style>
+	)
+}
+
+export default GlobalStyle
